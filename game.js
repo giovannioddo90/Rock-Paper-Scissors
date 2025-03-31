09ce2f00b10ed5a814b93af085721bf834e8b15c
Gio Oddo <giovannioddo90@gmail.com>
2025-03-31 12:32:28 -0400

    finished getHumanChoice() and initialized score variables

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-console.log("Hello world");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChioce() {
   switch (Math.floor(Math.random() * 3)) {
@@ -21,4 +22,20 @@ function getComputerChioce() {
 
 console.log(getComputerChioce());
 
+function getHumanChoice() {
+  //ask user for valid input 1 2 or 3, rock/paper/scissors
+  const choice = Number(window.prompt('1 - rock, 2 - paper, 3 - scissors', ''));
+  //switch/case for input
+  switch(choice) {
+    case 1: 
+      return 'rock';
+      break;
+    case 2: 
+      return 'paper';
+      break;
+    case 3:
+      return 'scissors'
+      break;
+  }
+}
 
